c1a4e7637cfa729a69b8d7e3e742fba4e45f4eb6
April LaRosa <95447994+aaprilfoolss@users.noreply.github.com>
2023-04-13 12:10:13 -0500

    adjustments

--- testscript.js
+++ testscript.js
@@ -33,7 +33,7 @@ window.addEventListener("load", init);
 window.addEventListener( "pageshow", function ( event ) {
     var historyTraversal = event.persisted || 
       ( typeof window.performance != "undefined" && 
-      window.performance.navigation.type === 2 );
+      window.performance.getEntriesByType("navigation")[0].type === "back_forward");
     if ( historyTraversal ) {
       // Handle page restore.
       window.location.reload();
