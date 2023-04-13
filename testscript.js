80e1818387e41ee97366dee9173cee1d8eaa559d
April LaRosa <95447994+aaprilfoolss@users.noreply.github.com>
2023-04-13 11:40:15 -0500

    back arrow reload

--- testscript.js
+++ testscript.js
@@ -29,3 +29,13 @@ function result() {
 }
 
 window.addEventListener("load", init);
+
+window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || 
+      ( typeof window.performance != "undefined" && 
+      window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+      // Handle page restore.
+      window.location.reload();
+    }
+  });
